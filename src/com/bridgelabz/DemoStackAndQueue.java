a0f3177f1962a630334017e4a2fb357d76d1f622
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 21:58:36 +0530

    Add: Code for dequeue from queue is adde.

--- src/com/bridgelabz/DemoStackAndQueue.java
+++ src/com/bridgelabz/DemoStackAndQueue.java
@@ -10,7 +10,9 @@ public class DemoStackAndQueue {
     numbers.add(56);
     numbers.add(30);
     numbers.add(70);
-
     System.out.println(numbers);
+
+    numbers.poll();
+        System.out.println(numbers);
     }
 }
