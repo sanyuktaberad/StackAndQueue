ae9d2afbaa5d77011b51c0f1b74e040c8c37b7fc
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 20:32:19 +0530

    Add: Code for creating a stack is added.

---
+++ src/com/bridgelabz/DemoStackAndQueue.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class DemoStackAndQueue {
+    public static void main(String[] args) {
+        Queue<Integer> numbers = new LinkedList<>();
+        numbers.add(56);
+        numbers.add(30);
+        numbers.add(70);
+        System.out.println(numbers);
+    }
+}
