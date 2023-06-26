318ca2051686cecc871ef1344aeb7359650ef9a2
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 21:39:36 +0530

    Add: Code for peak and pop for a stack is added.

--- src/com/bridgelabz/DemoStackAndQueue.java
+++ src/com/bridgelabz/DemoStackAndQueue.java
@@ -6,10 +6,20 @@ import java.util.Stack;
 
 public class DemoStackAndQueue {
     public static void main(String[] args) {
-        Queue<Integer> numbers = new LinkedList<>();
-        numbers.add(56);
-        numbers.add(30);
-        numbers.add(70);
+        Stack<Integer> numbers = new Stack<>();
+        numbers.push(56);
+        numbers.push(30);
+        numbers.push(70);
         System.out.println(numbers);
+
+        for (int i =0; i<=numbers.size();i++){
+            if (numbers.get(i) != 0){
+                numbers.pop();
+            }
+            System.out.println(numbers);
+        }
+        numbers.pop();
+        System.out.println(numbers);
+        System.out.println(numbers.isEmpty());
     }
 }
